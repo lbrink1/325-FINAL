6fb0b2548689d8efb17d9abfe4da889df664bb5a
lbrink1 <loren.brink@stonybrook.edu>
2021-11-24 18:04:13 -0500

    Added to the learning game (easy + hard level). Need to work on changing location of certain things because easy level works but hard does not? Also can't play again? I forget what I changed but check on this..

--- 325final.py
+++ 325final.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 
 ## TRANSLATOR CODE: LOREN
-        
+  
+#random word generator library 
+import random
+     
 #Using dictionaries for translation A-Z, 0-9
 translation = {"A": ".-",
                "B": "-...",
@@ -66,7 +69,7 @@ def translate_english(english):
     return morsefromenglish
 
 # Prompts user to input english to translate
-english = input("Enter English to translate to morse code:")
+english = input("Enter English to translate to morse code: ")
     
 #Call translator to translate english to morse code
 morsefromenglish = translate_english(english)   
@@ -77,11 +80,94 @@ print(morsefromenglish)
 #####################################################
 
 
-## GAME CODE : SEBASTIAN/LORhEN
+## GAME CODE : LOREN / SEBASTIAN
+difficulty = input("What level of difficulty would you like to play? Please enter easy or hard: ")
 
 
+while difficulty == 'easy':
+    
+    learningeasy= {"-.-- . ...":"yes",
+                   "-.. --- --.":"dog" ,
+                   "-.-. .- -":"cat",
+                   "... . .-":"sea",
+                   "..-. --- -..-":"fox",
+                   "- .-. . .":"tree",
+                    ".-. .- - .":"rate",
+                    "- .... .":"the",
+                    ".- -. -..":"and",
+                    "-. ---":"no",
+                    "-.- . -.--":"key",
+                    ".-- .... -.--":"why",
+                    "... ..- -.": "sun"
+                    }
 
+    easy = list(learningeasy.keys())
+    random.shuffle(easy) 
+    
+    correctanswers = 0
+    wronganswers = 0
+    
+    for keyword in easy:
+        rightwrong = "{}"
+        print(rightwrong.format(keyword))
+        answer= input("What is this in English? Enter in all lowercase: ")
+        print(" ")
+        
+        if answer == (learningeasy[keyword]):
+            print("Correct answer!")
+            correctanswers += 1
+        else: 
+            print("Wrong answer.")
+            wronganswers += 1
+            
+    rightwrong = "Your score is {}/{}"
+
+    print(rightwrong.format(correctanswers,wronganswers))  
+      
+    break
+   
+else:
+    
+    learninghard = {"... -.-. .... --- --- .-..": "school", 
+                    ".--. .-. --- .--- . -.-. -":"project",
+                    "..-. .. ... ....":"fish", 
+                    "-- --- - .... . .-.": "mother", 
+                    "..-. .- - .... . .-.":"father",
+                    "..-. .. -. .- .-..":"final", 
+                    "... -.-. --- .-. .":"score",
+                    "- .. .-. . -..":"tired",
+                    "... .-.. . . .--.":"sleep",
+                    "--. .-. . . -.":"green",
+                    "... -.- .- - .":"skate",
+                    "-... .-. . .- -.-":"break",
+                    "... ..- -. -.. .- -.--":"sunday",
+                
+                 }
+
+hard = list(learninghard.keys())
+
+random.shuffle(hard) 
+    
+correct_answers = 0
+wrong_answers = 0
+    
+for keyword in hard:
+    right_wrong = "{}"
+    print(right_wrong.format(keyword))
+    answer= input("What is this in English? Enter in all lowercase: ")
+        
+    if answer == (learninghard[keyword]):
+        print("Correct answer!")
+        correct_answers += 1
+    else: 
+        print("Wrong answer.")
+        wrong_answers += 1
+            
+    right_wrong = "Your score is {}/{}"
+    
+print(right_wrong.format(correct_answers,wrong_answers))  
+difficulty = input("Want to try again? To continue, enter 'easy' or 'hard'. If you recieve a perfect score of 13/13 on 'easy' mode, type 'hard' to move on to a more difficult level: ")
+
+    
+        
 ## GUI CODE: SEBASTIAN
-
-
-
